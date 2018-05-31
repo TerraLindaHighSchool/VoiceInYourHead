9728499680085a4c98f5139405d3149dad1aae77
Bruce Gustin <bgustin@srcs.org>
2018-05-31 14:49:30 -0700

    Main has List of Scenes but needs Switch

--- MainActivity.java
+++ MainActivity.java
@@ -15,10 +15,25 @@ public class MainActivity
         System.out.println("Before we start the game,what would you like us to call you?");
         String name = userInput.nextLine();
         System.out.println("Please choose a starting scene.");
-        System.out.println("1. Tangerine\n" + 
-                           "2. Forge Weapon\n" +
-                           "3. To be determined\n" +
-                           "4. To be determined\n");           
+        System.out.println("1. Introduction to Game\n" + 
+                           "2. Forge Weapons\n" +
+                           "3. Tangerine\n" +
+                           "4. Nobility Rallying\n" + 
+                           "5. Talk to the People\n" +
+                           "6. Defense\n" +
+                           "7. Peasent Rallying\n" + 
+                           "8. Cipher Note\n" +
+                           "9. Sinking Ship\n" +
+                           "10. Escape the Bear\n" + 
+                           "11. Puzzle Castle\n" +
+                           "12. Find Help\n" +
+                           "13. Know the Weather\n" +
+                           "14. Riddle\n" + 
+                           "15. Bar Fight\n" +
+                           "16. Mystical Weapon\n" +
+                           "17. Dialog\n" + 
+                           "18. Solve Riddle\n");  
+                           
         Playable player;
             switch(userInput.nextInt()) {
             case 1: player = new OpeningScene(name);
@@ -28,6 +43,22 @@ public class MainActivity
             case 3: player = new OpeningScene(name);
                     break;
             case 4: player = new OpeningScene(name);
+                    break;
+            case 6: player = new OpeningScene(name);
+                    break;
+            case 7: player = new OpeningScene(name);
+                    break;
+            case 8: player = new OpeningScene(name);
+                    break;
+            case 9: player = new OpeningScene(name);
+                    break;
+            case 10: player = new OpeningScene(name);
+                    break;
+            case 11: player = new OpeningScene(name);
+                    break;
+            case 12: player = new OpeningScene(name);
+                    break;
+            case 13: player = new OpeningScene(name);
                     break;
             default: System.out.println("No such choice.");
         }
